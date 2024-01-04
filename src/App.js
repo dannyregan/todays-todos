0ce49046abc9e88b82002b6c2d4bd87ef6fe09b5
Danny Regan <d.regan@columbia.edu>
2024-01-04 16:10:42 -0500

    set up files and create button, header components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header'
+import Button from './Button'
 
 function App() {
+  const handleSubmit = () => {
+    console.log('Submitted')
+  }
+
+  const handleDelete = () => {
+    console.log('Deleted')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Today's To Dos"/>
+      <Button value='Submit' onClick={handleSubmit} />
+      <Button value='Delete' onClick={handleDelete} />
     </div>
   );
 }
